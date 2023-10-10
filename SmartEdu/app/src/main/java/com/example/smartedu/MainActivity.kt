683121fb16e9b1430040f3683fb92493a07c3a0c
Faldin26 <faqihnoor@gmail.com>
2023-10-10 22:01:32 +0700

    second commit

--- SmartEdu/app/src/main/java/com/example/smartedu/MainActivity.kt
+++ SmartEdu/app/src/main/java/com/example/smartedu/MainActivity.kt
@@ -6,11 +6,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.smartedu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var tv:TextView
+    private lateinit var binding: ActivityMainBinding
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(androidx.fragment.R.id.fragment_container_view_tag,fragment)
+        fragmentTransaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv = findViewById(R.id.textView2)
 
@@ -20,17 +29,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnImplicit: Button = findViewById(R.id.btn_implict)
         btnImplicit.setOnClickListener(this)
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRed.setOnClickListener { replaceFragment(Fragment_1()) }
+        binding.btnBlue.setOnClickListener { replaceFragment(Fragment_2()) }
+
     }
 
     override fun onClick(v: View) {
         when (v.id){
             R.id.btn_implict -> {
-//                val message = "Sharing"
-//                val intent = Intent()
-//                intent.action = Intent.ACTION_SEND
-//                intent.putExtra(Intent.EXTRA_TEXT, message)
-//                intent.type = "text/plain"
-//                startActivity(intent)
 
                 val intent = Intent()
                 intent.putExtra("history", "Anda sudah login")
